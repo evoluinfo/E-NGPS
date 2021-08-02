79f64a41dfd10244328c57a2f146d8bd94ef9de3
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-08-01 23:11:28 -0300

    Add routes

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 from django.urls import path
+from login import views
+
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="login.html"), name="login")
+    path('', views.logar_usuario, name="login"),
+    path('app/deslogar_usuario', views.deslogar_usuario, name="deslogar_usuario"),
+    path('cadastrar_usuario', views.cadastrar_usuario, name="cadastrar_usuario"),
 ]
