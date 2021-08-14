7706611359fc28ec62a09b68c4a8b6f839b01a28
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-08-14 15:44:38 -0300

    adjusted config static files

--- main/settings.py
+++ main/settings.py
@@ -107,9 +107,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/base/static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'base/static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
