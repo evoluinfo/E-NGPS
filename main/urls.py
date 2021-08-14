cec03920af9ec6161140fc373068b2822e0eb72d
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-08-14 15:45:06 -0300

    ajusted config urls

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("login.urls")),
     path("app/", include("base.urls")),
+    path("app/client/", include("client.urls")),
     path('admin/', admin.site.urls),
 ]
